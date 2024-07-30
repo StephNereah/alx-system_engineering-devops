1543185e6aa7a152b7d7d3f2da24ad1eb440b96e
StephNereah <stphnadhiambo@gmail.com>
2024-07-30 11:29:09 -0700

    fix attempt package failed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # This Puppet manifest ensures that Flask version 2.1.0 is installed using pip3.
 
 package { 'flask':
-    ensure  => 'pip3',
-    content => '2.1.0',
+    ensure  => '2.1.0',
+    provider => 'pip3',
 }
