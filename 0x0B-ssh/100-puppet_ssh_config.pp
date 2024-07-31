b72b8d3ec135996c82fec639ce09a5efee6ee119
StephNereah <stphnadhiambo@gmail.com>
2024-07-30 17:18:07 -0700

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,23 +1,13 @@
 # Puppet manifest to configure SSH client to use a specific private key and disable password authentication
 
-# Ensure the SSH client configuration file exists
-file { '/etc/ssh/ssh_config':
-  ensure => file,
-  mode   => '0644',
-}
-
-# Add a configuration line to use the specified private key
 file_line { 'Declare identity file':
   path  => '/etc/ssh/ssh_config',
   line  => 'IdentityFile ~/.ssh/school',
   match => '^IdentityFile',
-  ensure => present,
 }
 
-# Disable password authentication
 file_line { 'Turn off passwd auth':
   path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
   match => '^PasswordAuthentication',
-  ensure => present,
 }
