f057631080523818ca23ba0fbed8ec9c45e84df7
StephNereah <stphnadhiambo@gmail.com>
2024-08-18 15:46:31 -0700

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,18 +1,7 @@
-# Puppet manifest to fix the Apache issue
+# This Puppet script fixes the Apache 500 error by adjusting file permissions.
 
-exec { 'fix-apache-permissions':
-  command => '/bin/chmod -R 755 /var/www/html',
-  onlyif  => '/usr/bin/test -d /var/www/html',
-}
-
-file { '/etc/apache2/sites-available/000-default.conf':
-  ensure  => file,
-  mode    => '0644',
-  content => template('apache/000-default.conf.erb'),
-  notify  => Exec['restart-apache'],
-}
-
-exec { 'restart-apache':
-  command     => '/bin/systemctl restart apache2',
-  refreshonly => true,
+exec { 'replace_phpp_with_php':
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  provider => shell,
+  onlyif   => 'grep "phpp" /var/www/html/wp-settings.php',
 }
