eeff2274a5400b12c53a47a46086daa39fde500e
StephNereah <stphnadhiambo@gmail.com>
2024-08-21 07:57:45 -0700

    Trial

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,24 +6,18 @@ and returns the number of subscribers for a given subreddit.
 Return 0 if the subreddit is invalid.
 """
 import requests
-#import sys
 
 
 def number_of_subscribers(subreddit):
-    """Queries to reddit API"""
-    url_base = 'https://www.reddit.com/r/'
-
-    url = '{}{}/about.json'.format(url_base, subreddit)
+    """Queries the Reddit API for number of subscribers."""
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
     headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
+        'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.2.3) \
         Gecko/20100401 Firefox/3.6.3 (FM Scene 4.6.1)'
     }
-    results = requests.get(
-        url,
-        headers=headers,
-        allow_redirects=False
-    )
-    if results.status_code == 200:
-        return results.json()['data']['subscribers']
-    return 0
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        return response.json().get('data', {}).get('subscribers', 0)
+    else:
+        return 0
